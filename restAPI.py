7b7a89b104d1005e8b789a164fb96b7bdb691af1
edward.zheng <edward.zheng@ringcentral.com>
2018-10-03 13:27:17 +0800

    :bug: port binding

--- restAPI.py
+++ restAPI.py
@@ -39,4 +39,5 @@ def index():
 
 if __name__ == '__main__':
 	from os import environ
-	app.run(host = '0.0.0.0',port=environ.get("PORT", 6789))
+	port = int(environ.get('PORT',6789))
+	app.run(host = '0.0.0.0',port=port)
